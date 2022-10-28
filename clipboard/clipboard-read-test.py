8cb25c4c2e8de850b8afc825d2441245c8a18e57
BradTFT <bradtft@outlook.com>
2022-10-28 12:28:48 -0400

    Add working clipboard reader

---
+++ clipboard/clipboard-read-test.py
@@ -0,0 +1,9 @@
+
+from tkinter import Tk  # Python 3
+
+x = Tk().clipboard_get()
+print(x) 
+#this needs to be set up to read the text(TTS) instead of printing.
+# but we are reading the clipboard whicih is good
+
+#!works
