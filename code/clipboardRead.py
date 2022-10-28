75b447f871ddc5382f24647a1d70c9495874d464
BradTFT <bradtft@outlook.com>
2022-10-28 12:49:57 -0400

    Rename and move files

--- code/clipboardRead.py
+++ code/clipboardRead.py
@@ -1,8 +1,9 @@
 
-from tkinter import Tk  # Python 3
 
-x = Tk().clipboard_get()
-print(x) 
+def GetClipboard():
+    from tkinter import Tk
+    storedClipboard = Tk().clipboard_get()
+    
 #this needs to be set up to read the text(TTS) instead of printing.
 # but we are reading the clipboard whicih is good
 
